688d845915bd752e2a864a1273f6455d08ee67fa
RaphCodec <46251144+RaphCodec@users.noreply.github.com>
2024-01-15 07:32:27 -0500

    Fixed issue where primary key columns were nullable

--- ETLFlow.py
+++ ETLFlow.py
@@ -48,36 +48,39 @@ def CreateTable(df,
     dtypes = df.dtypes
 
     # Create the CREATE TABLE statement
-    create_table_query = f"CREATE TABLE {table} (\n"
+    create_table_query = f"CREATE TABLE [{table}] (\n"
 
     for column, dtype in zip(columns, dtypes):
-        sql_type = sql_data_types.get(str(dtype), 'VARCHAR(max)') #defaulting to VARCHAR(max) if no datatype is matched
+        sql_type = sql_data_types.get(str(dtype), 'VARCHAR(max)')  # defaulting to VARCHAR(max) if no datatype is matched
         if dtype == 'object':
             max_length = df[column].astype(str).apply(len).max() + charbuff
-            create_table_query += f"    {column} {sql_type}({max_length}),\n"
+            create_table_query += f"    [{column}] {sql_type}({max_length}),\n"
         else:
-            create_table_query += f"    {column} {sql_type},\n"
+            create_table_query += f"    [{column}] {sql_type},\n"
+        if column in primary or column == primary: #ensures that primary key columans are not null
+            create_table_query = create_table_query[:-2]
+            create_table_query += " NOT NULL,\n"
 
     create_table_query = create_table_query.rstrip(',\n') + "\n);"
 
     # Adding primary key(s) if needed
     if primary is not None:
-        primary_key_constraint = f'CONSTRAINT {primaryName} PRIMARY KEY ' if primaryName else 'PRIMARY KEY '
-        primary_keys = primary if isinstance(primary, str) else ", ".join(primary)
-        create_table_query += f'\nALTER TABLE {table}\nADD {primary_key_constraint}({primary_keys});\n'
+        primary_key_constraint = f'CONSTRAINT [{primaryName}] PRIMARY KEY ' if primaryName else 'PRIMARY KEY '
+        primary_keys = primary if isinstance(primary, str) else ", ".join([f'[{key}]' for key in primary])
+        create_table_query += f'\nALTER TABLE [{table}]\nADD {primary_key_constraint}({primary_keys});\n'
 
     # Adding foreign key(s) if needed
     if foreign is not None and foreignName is not None and foreignTable is not None and foreignRelated is not None:
-        foreign_key_constraint = f'CONSTRAINT {foreignName} FOREIGN KEY ' if foreignName else 'FOREIGN KEY '
-        foreign_keys = foreign if isinstance(foreign, str) else ", ".join(foreign)
-        foreign_related_keys = foreignRelated if isinstance(foreignRelated, str) else ", ".join(foreignRelated)
-        create_table_query += f'\nALTER TABLE {table}\nADD {foreign_key_constraint}({foreign_keys})\nREFERENCES {foreignTable} ({foreign_related_keys});\n'
+        foreign_key_constraint = f'CONSTRAINT [{foreignName}] FOREIGN KEY ' if foreignName else 'FOREIGN KEY '
+        foreign_keys = foreign if isinstance(foreign, str) else ", ".join([f'[{key}]' for key in foreign])
+        foreign_related_keys = foreignRelated if isinstance(foreignRelated, str) else ", ".join([f'[{key}]' for key in foreignRelated])
+        create_table_query += f'\nALTER TABLE [{table}]\nADD {foreign_key_constraint}({foreign_keys})\nREFERENCES [{foreignTable}] ({foreign_related_keys});\n'
 
     # Adding unique key(s) if needed
     if unique is not None:
-        unique_constraint = f'CONSTRAINT {uniqueName} UNIQUE ' if uniqueName else 'UNIQUE '
-        unique_keys = unique if isinstance(unique, str) else ", ".join(unique)
-        create_table_query += f'\nALTER TABLE {table}\nADD {unique_constraint}({unique_keys});\n'
+        unique_constraint = f'CONSTRAINT [{uniqueName}] UNIQUE ' if uniqueName else 'UNIQUE '
+        unique_keys = unique if isinstance(unique, str) else ", ".join([f'[{key}]' for key in unique])
+        create_table_query += f'\nALTER TABLE [{table}]\nADD {unique_constraint}({unique_keys});\n'
 
     return create_table_query
 

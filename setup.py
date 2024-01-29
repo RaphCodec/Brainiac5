bc3c443bb06b4129ad175dc0f808ef47a95c4d4e
RaphCodec <46251144+RaphCodec@users.noreply.github.com>
2024-01-28 19:44:14 -0500

    Fixed files so pip install works and reorganized

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
+VERSION = '0.0.1' 
+DESCRIPTION = 'PYODBC and Pandas helper module'
+LONG_DESCRIPTION = 'Library to help with commonn ETL tasks with sql and pandas'
+
+# Setting up
 setup(
-    name='ETLFlow',
-    packages=find_packages(include=['ETLFlow']),
-    version='0.1.0',
-    description='Python helper library desinged to work with Pandas and PYODBC. Main use is to help with ETL work.',
-    author='RaphCodec',
-    install_requires=['pandas==2.0.3','pyodbc==4.0.39'],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest==4.4.1'],
-    test_suite='tests',
+       # the name must match the folder name 'verysimplemodule'
+        name="ETLFlow", 
+        version=VERSION,
+        author="Rapahel Cilfton",
+        author_email="",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(include=['ETLFlow']),
+        install_requires=['pandas==2.0.3','pyodbc==4.0.39'], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python', 'ETL'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
 )

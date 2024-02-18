89c0d05203f37d1578e07866297478afb7bc84bd
RaphCodec <46251144+RaphCodec@users.noreply.github.com>
2024-02-18 12:34:32 -0500

    Updated init and setup py files

--- setup.py
+++ setup.py
@@ -9,21 +9,18 @@ setup(
        # the name must match the folder name 'verysimplemodule'
         name="ETLFlow", 
         version=VERSION,
-        author="Rapahel Cilfton",
+        author="Rapahel Cilfton (Github: RaphCodeC)",
         author_email="",
         description=DESCRIPTION,
         long_description=LONG_DESCRIPTION,
         packages=find_packages(include=['ETLFlow']),
-        install_requires=['pandas==2.0.3','pyodbc==4.0.39', 'tqdm==4.66.1'], # add any additional packages that 
+        install_requires=['pandas>=2.0.3','pyodbc>=4.0.39', 'tqdm>=4.66.1'], # add any additional packages that 
         # needs to be installed along with your package. Eg: 'caer'
         
         keywords=['python', 'ETL'],
         classifiers= [
-            "Development Status :: 3 - Alpha",
-            "Intended Audience :: Education",
-            "Programming Language :: Python :: 2",
+            "Intended Audience :: Developers",
             "Programming Language :: Python :: 3",
-            "Operating System :: MacOS :: MacOS X",
             "Operating System :: Microsoft :: Windows",
         ]
 )
